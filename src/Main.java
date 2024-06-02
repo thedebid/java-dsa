3780ee9fc4047f94161a22d2072e2c0d2f301d7d
Debid Magar <linkwithdm@gmail.com>
2024-06-02 23:09:39 +0545

    #LSPPDay2 - DSAWithJava: Queue

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,74 @@
+import queues.ArrayQueue;
 import stacks.ArrayStack;
-import stacks.Employee;
+//import stacks.Employee;
+import queues.Employee;
 import stacks.LinkedStack;
 
 public class Main {
-    public static void main(String[] args) {
-        //Stack impl with Array
-        ArrayStack stack = new ArrayStack(10);
+    public static void main(String[] args) throws NoSuchMethodException {
+//        //Stack impl with Array
+//        ArrayStack stack = new ArrayStack(10);
+//
+//        stack.push(new Employee("Jane", "Jones", 123));
+//        stack.push(new Employee("John", "Doe", 4567));
+//        stack.push(new Employee("Mary", "Smith", 22));
+//        stack.push(new Employee("Mike", "Wilson", 3245));
+//        stack.push(new Employee("Bill", "End", 78));
+//
+//        //stack.printStack();
+//
+//        System.out.println(stack.peek());
+//        //stack.printStack();
+//
+//        System.out.println("Popped: " + stack.pop());
+//        System.out.println(stack.peek());
+//
+//
+//
+//
+//        //Stack impl with LinkedList
+//        Employee janeJones = new Employee("Jane", "Jones", 123);
+//        Employee johnDoe = new Employee("John", "Doe", 4567);
+//        Employee marySmith = new Employee("Mary", "Smith", 22);
+//        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+//        Employee billEnd = new Employee("Bill", "End", 78);
+//
+//        LinkedStack linkedStack = new LinkedStack();
+//        linkedStack.push(janeJones);
+//        linkedStack.push(johnDoe);
+//        linkedStack.push(marySmith);
+//        linkedStack.push(mikeWilson);
+//        linkedStack.push(billEnd);
+//
+//        //stack.printStack();
+//
+//        //System.out.println(stack.peek());
+//        //stack.printStack();
+//
+//        System.out.println("Popped: " + linkedStack.pop());
+//        System.out.println(linkedStack.peek());
 
-        stack.push(new Employee("Jane", "Jones", 123));
-        stack.push(new Employee("John", "Doe", 4567));
-        stack.push(new Employee("Mary", "Smith", 22));
-        stack.push(new Employee("Mike", "Wilson", 3245));
-        stack.push(new Employee("Bill", "End", 78));
 
-        //stack.printStack();
-
-        System.out.println(stack.peek());
-        //stack.printStack();
-
-        System.out.println("Popped: " + stack.pop());
-        System.out.println(stack.peek());
-
-
-
-
-        //Stack impl with LinkedList
+        //Queue impl with Array
         Employee janeJones = new Employee("Jane", "Jones", 123);
         Employee johnDoe = new Employee("John", "Doe", 4567);
         Employee marySmith = new Employee("Mary", "Smith", 22);
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
         Employee billEnd = new Employee("Bill", "End", 78);
 
-        LinkedStack linkedStack = new LinkedStack();
-        linkedStack.push(janeJones);
-        linkedStack.push(johnDoe);
-        linkedStack.push(marySmith);
-        linkedStack.push(mikeWilson);
-        linkedStack.push(billEnd);
+        ArrayQueue queue =  new ArrayQueue(10);
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.add(marySmith);
+        queue.add(mikeWilson);
+        queue.add(billEnd);
 
-        //stack.printStack();
 
-        //System.out.println(stack.peek());
-        //stack.printStack();
-
-        System.out.println("Popped: " + linkedStack.pop());
-        System.out.println(linkedStack.peek());
+//        queue.printQueue();
+        queue.remove();
+//        queue.printQueue();
+        System.out.println(queue.peek());
+        queue.printQueue();
 
     }
 }
---
+++ src/queues/Theory.java
@@ -0,0 +1,10 @@
+package queues;
+
+public class Theory {
+    // Abstract data type
+    // FIFO - first in first out
+    // add - also called enqueue - add an item to the end of the queue
+    // remove - also called dequeue - remove the item  at the front of the queue
+    // peek - get the item at the front of the queue, but dont remove it
+
+}
